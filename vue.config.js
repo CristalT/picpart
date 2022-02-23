624ca537a0f26a6f63a96249149f4b4de9f2cb65
Marcelo J Forclaz <marcelo.forclaz@stensul.com>
2022-02-23 10:34:25 -0300

    new structure

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: path.join(__dirname, './src/resources/main.js'),
+    },
+  },
   pluginOptions: {
     electronBuilder: {
       preload: 'src/preload.js',
     },
   },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, './src/resources/'),
+      }
+    }
+  }
 });
